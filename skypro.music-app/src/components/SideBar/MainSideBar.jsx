0330132e2f96a11e579bff7cc0ad241186b5fac1
SuriMan0305 <mihail.levchenko.0305@mail.ru>
2023-10-10 16:37:12 +0300

    add loading animations

--- skypro.music-app/src/components/SideBar/MainSideBar.jsx
+++ skypro.music-app/src/components/SideBar/MainSideBar.jsx
@@ -1,49 +1,97 @@
+import React from "react";
 import "./sidebar.css";
 
+const { useEffect, useState } = React;
+
 const SidePanel = () => {
-  return (
-    <div className="main__sidebar sidebar">
-      <div className="sidebar__personal">
-        <p className="sidebar__personal-name">levchenko Kirill</p>
-        <div className="sidebar__icon">
-          <svg alt="logout">
-            <use xlinkHref="img/icon/sprite.svg#logout" />
-          </svg>
-        </div>
-      </div>
-      <div className="sidebar__block">
-        <div className="sidebar__list">
-          <div className="sidebar__item">
-            <a className="sidebar__link" href="#!">
-              <img
-                className="sidebar__img"
-                src="img/playlist01.png"
-                alt="day's playlist"
-              />
-            </a>
-          </div>
-          <div className="sidebar__item">
-            <a className="sidebar__link" href="#!">
-              <img
-                className="sidebar__img"
-                src="img/playlist02.png"
-                alt="day's playlist"
-              />
-            </a>
-          </div>
-          <div className="sidebar__item">
-            <a className="sidebar__link" href="#!">
-              <img
-                className="sidebar__img"
-                src="img/playlist03.png"
-                alt="day's playlist"
-              />
-            </a>
+  const [seconds, setSeconds] = useState(0);
+
+  const increase = () => {
+    setSeconds(seconds + 1);
+  };
+
+  useEffect(() => {
+    const TimerId = setInterval(() => {
+      if (seconds < 3) {
+        increase();
+      }
+    }, 1000);
+    return () => {
+      clearInterval(TimerId);
+    };
+  });
+
+  if (seconds === 3) {
+    return (
+      <div className="main__sidebar sidebar">
+        <div className="sidebar__personal">
+          <p className="sidebar__personal-name">levchenko Kirill</p>
+          <div className="sidebar__icon">
+            <svg alt="logout">
+              <use xlinkHref="img/icon/sprite.svg#logout" />
+            </svg>
           </div>
         </div>
+        <div className="sidebar__block">
+          <div className="sidebar__list">
+            <div className="sidebar__item">
+              <a className="sidebar__link" href="#!">
+                <img
+                  className="sidebar__img"
+                  src="img/playlist01.png"
+                  alt="day's playlist"
+                />
+              </a>
+            </div>
+            <div className="sidebar__item">
+              <a className="sidebar__link" href="#!">
+                <img
+                  className="sidebar__img"
+                  src="img/playlist02.png"
+                  alt="day's playlist"
+                />
+              </a>
+            </div>
+            <div className="sidebar__item">
+              <a className="sidebar__link" href="#!">
+                <img
+                  className="sidebar__img"
+                  src="img/playlist03.png"
+                  alt="day's playlist"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="main__sidebar sidebar">
+        <div className="sidebar__personal">
+          <p className="sidebar__personal-name">loading...</p>
+          <div className="sidebar__icon">
+            <svg alt="logout">
+              <use xlinkHref="img/icon/sprite.svg#logout" />
+            </svg>
+          </div>
+        </div>
+        <div className="sidebar__block">
+          <div className="sidebar__list">
+            <div className="sidebar__item_loading">
+              <div className="sidebar__preload"></div>
+            </div>
+            <div className="sidebar__item_loading">
+              <div className="sidebar__preload"></div>
+            </div>
+            <div className="sidebar__item_loading">
+              <div className="sidebar__preload"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export { SidePanel };
--- skypro.music-app/src/components/bottombar/SongInfo.jsx
+++ skypro.music-app/src/components/bottombar/SongInfo.jsx
@@ -1,39 +1,89 @@
+import React from "react";
 import "./bar.css";
 
+const { useEffect, useState } = React;
+
 const PlayingInfoSong = (props) => {
-  return (
-    <div className="player__track-play track-play">
-      <div className="track-play__contain">
-        <div className="track-play__image">
-          <svg className="track-play__svg" alt="music">
-            <use xlinkHref="img/icon/sprite.svg#icon-note" />
-          </svg>
+  const [seconds, setSeconds] = useState(0);
+
+  const increase = () => {
+    setSeconds(seconds + 1);
+  };
+
+  useEffect(() => {
+    const TimerId = setInterval(() => {
+      if (seconds < 3) {
+        increase();
+      }
+    }, 1000);
+    return () => {
+      clearInterval(TimerId);
+    };
+  });
+
+  if (seconds === 3) {
+    return (
+      <div className="player__track-play track-play">
+        <div className="track-play__contain">
+          <div className="track-play__image">
+            <svg className="track-play__svg" alt="music">
+              <use xlinkHref="img/icon/sprite.svg#icon-note" />
+            </svg>
+          </div>
+          <div className="track-play__author">
+            <a className="track-play__author-link" href="http://">
+              {props.song}
+            </a>
+          </div>
+          <div className="track-play__album">
+            <a className="track-play__album-link" href="http://">
+              {props.artist}
+            </a>
+          </div>
         </div>
-        <div className="track-play__author">
-          <a className="track-play__author-link" href="http://">
-            {props.song}
-          </a>
-        </div>
-        <div className="track-play__album">
-          <a className="track-play__album-link" href="http://">
-          {props.artist}
-          </a>
+        <div className="track-play__like-dis">
+          <div className="track-play__like _btn-icon">
+            <svg className="track-play__like-svg" alt="like">
+              <use xlinkHref="img/icon/sprite.svg#icon-like" />
+            </svg>
+          </div>
+          <div className="track-play__dislike _btn-icon">
+            <svg className="track-play__dislike-svg" alt="dislike">
+              <use xlinkHref="img/icon/sprite.svg#icon-dislike" />
+            </svg>
+          </div>
         </div>
       </div>
-      <div className="track-play__like-dis">
-        <div className="track-play__like _btn-icon">
-          <svg className="track-play__like-svg" alt="like">
-            <use xlinkHref="img/icon/sprite.svg#icon-like" />
-          </svg>
+    );
+  } else {
+    return (
+      <div className="player__track-play track-play">
+        <div className="track-play__contain">
+          <div className="track-play__image">
+            <div className="loading_bar__album"></div>
+          </div>
+          <div className="track-play__loading">
+              <div className="loading_bar__player"></div>
+          </div>
+          <div className="track-play__loading">
+            <div className="loading_bar__player"></div>
+          </div>
         </div>
-        <div className="track-play__dislike _btn-icon">
-          <svg className="track-play__dislike-svg" alt="dislike">
-            <use xlinkHref="img/icon/sprite.svg#icon-dislike" />
-          </svg>
+        <div className="track-play__like-dis">
+          <div className="track-play__like _btn-icon">
+            <svg className="track-play__like-svg" alt="like">
+              <use xlinkHref="img/icon/sprite.svg#icon-like" />
+            </svg>
+          </div>
+          <div className="track-play__dislike _btn-icon">
+            <svg className="track-play__dislike-svg" alt="dislike">
+              <use xlinkHref="img/icon/sprite.svg#icon-dislike" />
+            </svg>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
-export {PlayingInfoSong};
+export { PlayingInfoSong };
