7b6af826d6bb71498666fcafa72d2cdd29b16d9d
SuriMan0305 <mihail.levchenko.0305@mail.ru>
2023-10-19 02:32:32 +0300

    add app styles components

--- skypro.music-app/src/AppStyles.js
+++ skypro.music-app/src/AppStyles.js
@@ -1,4 +1,7 @@
-@font-face {
+import styled, {createGlobalStyle} from 'styled-components'
+
+export const AppStyle = createGlobalStyle`
+  @font-face {
   font-family: "StratosSkyeng";
   src: local("StratosSkyeng"), local("StratosSkyeng"),
     url("../public/fonts/StratosSkyeng.woff2") format("woff2"),
@@ -6,27 +9,23 @@
   font-weight: 400;
   font-style: normal;
 }
-
 * {
   margin: 0;
   padding: 0;
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
 }
-
 *:before,
 *:after {
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
 }
-
 a,
 a:visited {
   text-decoration: none;
   font-family: "StratosSkyeng", sans-serif;
   cursor: pointer;
 }
-
 button {
   cursor: pointer;
 }
@@ -42,54 +41,21 @@ body {
   font-family: "StratosSkyeng", sans-serif;
   color: #ffffff;
 }
-
-.wrapper {
+`
+export const Wrapper = styled.div`
   width: 100%;
   min-height: 100%;
   overflow: hidden;
   background-color: #383838;
-}
-
-.container {
+`
+export const Container = styled.div`
   max-width: 1920px;
   height: 100vh;
   margin: 0 auto;
   position: relative;
   background-color: #181818;
-}
-
-/* ._btn-text:hover {
-  border-color: #d9b6ff;
-  color: #d9b6ff;
-  cursor: pointer;
-} */
-
-._btn-icon:hover svg {
-  fill: transparent;
-  stroke: #acacac;
-  cursor: pointer;
-}
-
-/* ._btn-text:active {
-  border-color: #ad61ff;
-  color: #ad61ff;
-  cursor: pointer;
-} */
-
-._btn-icon:active svg {
-  fill: transparent;
-  stroke: #ffffff;
-  cursor: pointer;
-}
-
-._btn-icon:active .track-play__like-svg,
-._btn-icon:active .track-play__dislike-svg {
-  fill: #696969;
-  stroke: #ffffff;
-  cursor: pointer;
-}
-
-.main {
+`
+export const Main = styled.div`
   -webkit-box-flex: 1;
   -ms-flex: 1 1 auto;
   flex: 1 1 auto;
@@ -101,12 +67,11 @@ body {
   -webkit-box-pack: justify;
   -ms-flex-pack: justify;
   justify-content: space-between;
-}
-
-.main__centerblock {
+`
+export const MainCenterBlock = styled.div`
   width: auto;
   -webkit-box-flex: 3;
   -ms-flex-positive: 3;
   flex-grow: 3;
   padding: 20px 40px 20px 111px;
-}
+`
