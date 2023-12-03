d5c3e35bacf16b934517356b6f2c1bf4b45d3eb7
SuriMan0305 <mihail.levchenko.0305@mail.ru>
2023-12-03 19:58:49 +0300

    1.1 next/prev ready

--- skypro.music-app/src/components/bottombar/player/Player.jsx
+++ skypro.music-app/src/components/bottombar/player/Player.jsx
@@ -2,7 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import { PlayingInfoSong } from "../info/SongInfo";
 import * as S from "../PlayeStyles.js";
 import { useDispatch } from "react-redux";
-import { startHandlePlay } from "../../../store/slices/playerActions.js";
+import {
+  nextButton,
+  prevButton,
+  startHandlePlay,
+} from "../../../store/slices/playerActions.js";
 
 export const PlayerPanel = ({ trackNow, playing, setPlaying }) => {
   const [volume, setVolume] = useState(0.5);
@@ -15,17 +19,21 @@ export const PlayerPanel = ({ trackNow, playing, setPlaying }) => {
   const inputRef = useRef(null);
   const audioRef = useRef(null);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleStart = () => {
     if ((!ending && playing) || (ending && !playing)) {
       audioRef.current.pause();
       setPlaying(false);
-      dispatch(startHandlePlay({...trackNow, statusPlay: !trackNow.statusPlay}))
+      dispatch(
+        startHandlePlay({ ...trackNow, statusPlay: !trackNow.statusPlay })
+      );
     } else if ((!ending && !playing) || (ending && playing)) {
       audioRef.current.play();
       setPlaying(true);
-      dispatch(startHandlePlay({...trackNow, statusPlay: !trackNow.statusPlay}))
+      dispatch(
+        startHandlePlay({ ...trackNow, statusPlay: !trackNow.statusPlay })
+      );
     }
   };
 
@@ -70,8 +78,15 @@ export const PlayerPanel = ({ trackNow, playing, setPlaying }) => {
     }`;
   };
 
-  useEffect(() => {
+  const nextPlay = () => {
+    dispatch(nextButton(trackNow));
+  };
 
+  const prevPlay = () => {
+    dispatch(prevButton(trackNow))
+  }
+
+  useEffect(() => {
     if (!playing) {
       audioRef.current.pause();
     } else {
@@ -79,7 +94,7 @@ export const PlayerPanel = ({ trackNow, playing, setPlaying }) => {
     }
 
     if (volume === 0.5) {
-        volumeRef.current.style.setProperty("--background-size", `50%`);
+      volumeRef.current.style.setProperty("--background-size", `50%`);
     }
     audioRef.current.volume = volume;
     const getInfoFunc = () => {
@@ -92,163 +107,172 @@ export const PlayerPanel = ({ trackNow, playing, setPlaying }) => {
 
       timeSong.addEventListener("timeupdate", () => {
         setNowTime(timeSong.currentTime);
-          inputRef.current.style.setProperty(
-            "--background-size",
-            `${inputRef.current.value}%`
-          );
+        inputRef.current.style.setProperty(
+          "--background-size",
+          `${inputRef.current.value}%`
+        );
         setEnding(audioRef.current.ended);
       });
       if (ending) {
-        setPlaying(false)
-        dispatch(startHandlePlay({ ...trackNow, statusPlay: false }))
+        setPlaying(false);
+        dispatch(startHandlePlay({ ...trackNow, statusPlay: false }));
+        setTimeout(() => {
+          setPlaying(true);
+          dispatch(startHandlePlay({ ...trackNow, statusPlay: true }));
+          dispatch(nextButton(trackNow));
+        }, 10);
       }
     };
 
     return getInfoFunc();
   }, [nowTime, length, playing, isLoop, ending, inputRef, audioRef]);
 
-    return (
-      <S.Bar>
-        <S.Timer>{timer(nowTime, length)}</S.Timer>
+  return (
+    <S.Bar>
+      <S.Timer>{timer(nowTime, length)}</S.Timer>
+      <div>
         <div>
-          <div>
-            <S.Audio controls src={trackNow.track_file} ref={audioRef} autoPlay></S.Audio>
-          </div>
-          <S.Content>
-            <S.PlayerProgress
-              ref={inputRef}
-              type="range"
-              name="rangeTime"
-              min="0"
-              max={100}
-              onChange={(range) => {
-                audioRef.current.currentTime =
-                  (range.currentTarget.value * length) / 100;
-                range.currentTarget.style.setProperty(
-                  "--background-size",
-                  `${range.currentTarget.value}%`
-                );
-              }}
-              value={`${(100 * nowTime) / length}`}></S.PlayerProgress>
-            <S.PlayerBlock>
-              <S.BarPlayer>
-                <S.PlayerControls>
-                  <S.PrevButton>
-                    <S.PrevButtonSvg
-                      alt="prev"
-                      onClick={() => {
-                        alert("element of player in production");
-                      }}>
-                      <svg
-                        width="16"
-                        height="14"
-                        viewBox="0 0 16 14"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1 2V12.5" stroke="white" />
-                        <path d="M3 7L12.75 0.937823L12.75 13.0622L3 7Z" />
-                      </svg>
-                    </S.PrevButtonSvg>
-                  </S.PrevButton>
-                  <S.PlayButton onClick={handleStart}>
-                    <S.PlayButtonSvg alt="play">
-                      {playing && !ending ? (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="15"
-                          height="19"
-                          viewBox="0 0 15 19"
-                          fill="currentColor">
-                          <rect width="5" height="19" />
-                          <rect x="10" width="5" height="19" />
-                        </svg>
-                      ) : (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="15"
-                          height="20"
-                          viewBox="0 0 15 20"
-                          fill="currentColor">
-                          <path d="M15 10L-1.01012e-06 0.47372L-1.84293e-06 19.5263L15 10Z" />
-                        </svg>
-                      )}
-                    </S.PlayButtonSvg>
-                  </S.PlayButton>
-                  <S.NextButton>
-                    <S.NextButtonSvg
-                      alt="next"
-                      onClick={() => {
-                        alert("element of player in production");
-                      }}>
-                      <svg
-                        width="16"
-                        height="14"
-                        viewBox="0 0 16 14"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15 2V12.5" stroke="white" />
-                        <path d="M13 7L3.25 0.937823L3.25 13.0622L13 7Z" />
-                      </svg>
-                    </S.NextButtonSvg>
-                  </S.NextButton>
-                  <S.RepeatButton onClick={handleChangeLoop}>
-                    <S.RepeatButtonSvg alt="repeat">
-                      {!isLoop ? (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="18"
-                          height="12"
-                          viewBox="0 0 20 18"
-                          fill="currentColor">
-                          <path d="M10 3L5 0.113249V5.88675L10 3ZM7 14.5C3.96243 14.5 1.5 12.0376 1.5 9H0.5C0.5 12.5899 3.41015 15.5 7 15.5V14.5ZM1.5 9C1.5 5.96243 3.96243 3.5 7 3.5V2.5C3.41015 2.5 0.5 5.41015 0.5 9H1.5Z" />
-                          <path d="M10 15L15 17.8868V12.1132L10 15ZM13 3.5C16.0376 3.5 18.5 5.96243 18.5 9H19.5C19.5 5.41015 16.5899 2.5 13 2.5V3.5ZM18.5 9C18.5 12.0376 16.0376 14.5 13 14.5V15.5C16.5899 15.5 19.5 12.5899 19.5 9H18.5Z" />
-                        </svg>
-                      ) : (
-                        <use xlinkHref="/img/icon/sprite.svg#icon-repeat" />
-                      )}
-                    </S.RepeatButtonSvg>
-                  </S.RepeatButton>
-                  <S.ShuffleButton>
-                    <S.ShuffleButtonSvg
-                      alt="shuffle"
-                      onClick={() => {
-                        alert("element of player in production");
-                      }}>
-                      <use xlinkHref="/img/icon/sprite.svg#icon-shuffle" />
-                    </S.ShuffleButtonSvg>
-                  </S.ShuffleButton>
-                </S.PlayerControls>
-                <PlayingInfoSong song={trackNow.album} artist={trackNow.author} />
-              </S.BarPlayer>
-              <S.BarVolumeBlock>
-                <S.VolumeContent>
-                  <S.VolumeImage onClick={handleMute}>
-                    <S.VolumeSvg alt="volume">
-                      <use xlinkHref="/img/icon/sprite.svg#icon-volume" />
-                    </S.VolumeSvg>
-                  </S.VolumeImage>
-                  <S.VolumeProgress>
-                    <S.VolumeProgressLine
-                      ref={volumeRef}
-                      type="range"
-                      name="range"
-                      min="0"
-                      max="100"
-                      value={volume * 100}
-                      onChange={(range) => {
-                        handleChangeVolume(range.currentTarget.value);
-                        volumeRef.current.style.setProperty(
-                          "--background-size",
-                          `${range.currentTarget.value}%`
-                        );
-                      }}
-                    />
-                  </S.VolumeProgress>
-                </S.VolumeContent>
-              </S.BarVolumeBlock>
-            </S.PlayerBlock>
-          </S.Content>
+          <S.Audio
+            controls
+            src={trackNow.track_file}
+            ref={audioRef}
+            autoPlay></S.Audio>
         </div>
-      </S.Bar>
-    );
-  }
+        <S.Content>
+          <S.PlayerProgress
+            ref={inputRef}
+            type="range"
+            name="rangeTime"
+            min="0"
+            max={100}
+            onChange={(range) => {
+              audioRef.current.currentTime =
+                (range.currentTarget.value * length) / 100;
+              range.currentTarget.style.setProperty(
+                "--background-size",
+                `${range.currentTarget.value}%`
+              );
+            }}
+            value={`${(100 * nowTime) / length}`}></S.PlayerProgress>
+          <S.PlayerBlock>
+            <S.BarPlayer>
+              <S.PlayerControls>
+                <S.PrevButton>
+                  <S.PrevButtonSvg
+                    alt="prev"
+                    onClick={() => {
+                      prevPlay()
+                    }}>
+                    <svg
+                      width="16"
+                      height="14"
+                      viewBox="0 0 16 14"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1 2V12.5" stroke="white" />
+                      <path d="M3 7L12.75 0.937823L12.75 13.0622L3 7Z" />
+                    </svg>
+                  </S.PrevButtonSvg>
+                </S.PrevButton>
+                <S.PlayButton onClick={handleStart}>
+                  <S.PlayButtonSvg alt="play">
+                    {playing && !ending ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="15"
+                        height="19"
+                        viewBox="0 0 15 19"
+                        fill="currentColor">
+                        <rect width="5" height="19" />
+                        <rect x="10" width="5" height="19" />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="15"
+                        height="20"
+                        viewBox="0 0 15 20"
+                        fill="currentColor">
+                        <path d="M15 10L-1.01012e-06 0.47372L-1.84293e-06 19.5263L15 10Z" />
+                      </svg>
+                    )}
+                  </S.PlayButtonSvg>
+                </S.PlayButton>
+                <S.NextButton>
+                  <S.NextButtonSvg
+                    alt="next"
+                    onClick={() => {
+                      nextPlay();
+                    }}>
+                    <svg
+                      width="16"
+                      height="14"
+                      viewBox="0 0 16 14"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15 2V12.5" stroke="white" />
+                      <path d="M13 7L3.25 0.937823L3.25 13.0622L13 7Z" />
+                    </svg>
+                  </S.NextButtonSvg>
+                </S.NextButton>
+                <S.RepeatButton onClick={handleChangeLoop}>
+                  <S.RepeatButtonSvg alt="repeat">
+                    {!isLoop ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="18"
+                        height="12"
+                        viewBox="0 0 20 18"
+                        fill="currentColor">
+                        <path d="M10 3L5 0.113249V5.88675L10 3ZM7 14.5C3.96243 14.5 1.5 12.0376 1.5 9H0.5C0.5 12.5899 3.41015 15.5 7 15.5V14.5ZM1.5 9C1.5 5.96243 3.96243 3.5 7 3.5V2.5C3.41015 2.5 0.5 5.41015 0.5 9H1.5Z" />
+                        <path d="M10 15L15 17.8868V12.1132L10 15ZM13 3.5C16.0376 3.5 18.5 5.96243 18.5 9H19.5C19.5 5.41015 16.5899 2.5 13 2.5V3.5ZM18.5 9C18.5 12.0376 16.0376 14.5 13 14.5V15.5C16.5899 15.5 19.5 12.5899 19.5 9H18.5Z" />
+                      </svg>
+                    ) : (
+                      <use xlinkHref="/img/icon/sprite.svg#icon-repeat" />
+                    )}
+                  </S.RepeatButtonSvg>
+                </S.RepeatButton>
+                <S.ShuffleButton>
+                  <S.ShuffleButtonSvg
+                    alt="shuffle"
+                    onClick={() => {
+                      alert("element of player in production");
+                    }}>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-shuffle" />
+                  </S.ShuffleButtonSvg>
+                </S.ShuffleButton>
+              </S.PlayerControls>
+              <PlayingInfoSong song={trackNow.album} artist={trackNow.author} />
+            </S.BarPlayer>
+            <S.BarVolumeBlock>
+              <S.VolumeContent>
+                <S.VolumeImage onClick={handleMute}>
+                  <S.VolumeSvg alt="volume">
+                    <use xlinkHref="/img/icon/sprite.svg#icon-volume" />
+                  </S.VolumeSvg>
+                </S.VolumeImage>
+                <S.VolumeProgress>
+                  <S.VolumeProgressLine
+                    ref={volumeRef}
+                    type="range"
+                    name="range"
+                    min="0"
+                    max="100"
+                    value={volume * 100}
+                    onChange={(range) => {
+                      handleChangeVolume(range.currentTarget.value);
+                      volumeRef.current.style.setProperty(
+                        "--background-size",
+                        `${range.currentTarget.value}%`
+                      );
+                    }}
+                  />
+                </S.VolumeProgress>
+              </S.VolumeContent>
+            </S.BarVolumeBlock>
+          </S.PlayerBlock>
+        </S.Content>
+      </div>
+    </S.Bar>
+  );
+};
--- skypro.music-app/src/store/slices/playerActions.js
+++ skypro.music-app/src/store/slices/playerActions.js
@@ -3,11 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   data: [undefined],
   nowPlay: {
-    id: '',
-    album: '',
-    author: '',
-    track_file: '',
-    statusPlay: '',
+    id: "",
+    album: "",
+    author: "",
+    track_file: "",
+    statusPlay: "",
   },
 };
 
@@ -19,7 +19,61 @@ export const playlist = createSlice({
       state.data = action.payload; // данные из диспатча передаются в state
     },
     startHandlePlay: (state, action) => {
-      state.nowPlay = action.payload
+      state.nowPlay = action.payload;
+    },
+    nextButton: (state, action) => {
+      let rememberNow = action.payload;
+      if (rememberNow.id - 7 < state.data.length) {
+        let nextInfoTrack = state.data.map((el) => {
+          if (rememberNow.id + 1 === el.id) {
+            const nextTrack = [el.id, el.album, el.author, el.track_file, true];
+            return nextTrack;
+          }
+        });
+        let filter = nextInfoTrack.filter((el) => el !== undefined);
+        state.nowPlay = {
+          id: filter[0][0],
+          album: filter[0][1],
+          author: filter[0][2],
+          track_file: filter[0][3],
+          statusPlay: filter[0][4],
+        };
+      } else {
+        state.nowPlay = {
+          id: state.data[0].id,
+          album: state.data[0].album,
+          author: state.data[0].author,
+          track_file: state.data[0].track_file,
+          statusPlay: state.data[0].statusPlay,
+        };
+      }
+    },
+    prevButton: (state, action) => {
+      let rememberNow = action.payload;
+      if (rememberNow.id > state.data[0].id) {
+        let nextInfoTrack = state.data.map((el) => {
+          if (rememberNow.id - 1 === el.id) {
+            const nextTrack = [el.id, el.album, el.author, el.track_file, true];
+            return nextTrack;
+          }
+        });
+        let filter = nextInfoTrack.filter((el) => el !== undefined);
+        state.nowPlay = {
+          id: filter[0][0],
+          album: filter[0][1],
+          author: filter[0][2],
+          track_file: filter[0][3],
+          statusPlay: filter[0][4],
+        };
+      } else {
+        state.nowPlay = {
+          id: state.data[state.data.length - 1].id,
+          album: state.data[state.data.length - 1].album,
+          author: state.data[state.data.length - 1].author,
+          track_file: state.data[state.data.length - 1].track_file,
+          statusPlay: state.data[state.data.length - 1].statusPlay,
+        };
+      }
     },
     setNext: (state) => {
       // добавляем к счетчику и делаем всё неактивным
@@ -30,13 +84,16 @@ export const playlist = createSlice({
       // добавляем к счетчику и делаем всё неактивным
       // проверяем соответствие индекса и если совпадает с кликом меняем статус если счётчик сделать с 0 то получается по индексу будет опережать события
       state.data = state.data.map((element, index) => {
-        return index+1 === state.counter ? { ...element, status: true } : element;
+        return index + 1 === state.counter
+          ? { ...element, status: true }
+          : element;
       });
       // проверяем соответствие индекса и если совпадает с кликом меняем статус если счётчик сделать с 0 то получается по индексу будет опережать события
     },
   },
 });
 
-export const { setPlaylist, setNext, startHandlePlay } = playlist.actions; //Экспортируем для диспатча
+export const { setPlaylist, setNext, startHandlePlay, nextButton, prevButton } =
+  playlist.actions; //Экспортируем для диспатча
 
 export default playlist.reducer; // Экспортируем для store
